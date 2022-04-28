1528d79c9e445d6c3cc31c19898941c148cbda25
Josue Solis <josue1dario2solis3@gmail.com>
2022-04-28 01:15:49 -0300

    create class PublicationService

---
+++ src/main/java/com/system/blog/services/PublicationService.java
@@ -0,0 +1,9 @@
+package com.system.blog.services;
+
+import com.system.blog.dtos.PublicationDto;
+
+public interface PublicationService {
+
+    public PublicationDto create(PublicationDto publicationDto);
+    
+}
