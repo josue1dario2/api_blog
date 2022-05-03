5f582a6f5388ac99f04c46a005f20546dc10b1f4
Josue Solis <josue1dario2solis3@gmail.com>
2022-05-02 21:31:39 -0300

    create class user and role repository

---
+++ src/main/java/com/system/blog/repositories/UserRepository.java
@@ -0,0 +1,22 @@
+package com.system.blog.repositories;
+
+import com.system.blog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameOrEmail(String username,String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+}
