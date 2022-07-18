98dcd23584c9690b11fcd6d5ca798d647e4b01ad
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-17 23:18:55 -0300

    change the data type of id from long to integer of the publication repository class

--- src/main/java/com/system/blog/repositories/PublicationRepository.java
+++ src/main/java/com/system/blog/repositories/PublicationRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PublicationRepository extends JpaRepository<Publication,Long> {
+public interface PublicationRepository extends JpaRepository<Publication,Integer> {
 }
