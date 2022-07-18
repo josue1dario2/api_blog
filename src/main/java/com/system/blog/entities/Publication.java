f6bfdbc703f003bf09b28c1c75e097808660a7ff
Josue Solis <josue1dario2solis3@gmail.com>
2022-07-17 23:14:17 -0300

    change the data type of id from long to integer of the Publication class

--- src/main/java/com/system/blog/entities/Publication.java
+++ src/main/java/com/system/blog/entities/Publication.java
@@ -18,7 +18,7 @@ public class Publication {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer idPublication;
 
     @Column(name = "title",nullable = false)
     private String title;
